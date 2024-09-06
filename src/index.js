45b1cad169557297fc88838510c63cc3989132ab
JhonatanDev <jhonatan3506@gmail.com>
2024-09-06 10:25:45 -0500

    agregando cors especifico al proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan")
 const database=require("./database")
-
+const cors= require("cors");
 //Configuracion Inicial
 const app = express();
 app.set("port", 4000);
@@ -11,12 +11,14 @@ console.log("Escuchando en el puerto " + app.get("port"))
 
 //Moddlewares
 app.use(morgan("dev"))
-
+app.use(cors({
+    origin:["http://127.0.0.1:5501","http://127.0.0.1:5500"]
+}))
 
 //Rutas
 app.get("/productos", async (req, res) => {
     // res.send("Mensaje recibido nuevo log")
    const connection= await database.getConnection()
    const result= await connection.query("SELECT * from producto");
-   console.log(result)
+   res.json(result)
 })
